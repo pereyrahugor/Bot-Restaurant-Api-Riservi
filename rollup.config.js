6bb8e8998f6c369d04af3dad42f00bae23c96eb5
Pereyra Hugo <62813900+pereyrahugor@users.noreply.github.com>
2025-08-20 20:06:36 -0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,8 @@ import typescript from 'rollup-plugin-typescript2'
 export default {
     input: 'src/app.ts',
     output: {
-        dir: 'dist',           // <-- Usa esta opción
-        format: 'cjs',         // o el formato que uses
+        file: 'dist/app.js',
+        format: 'esm',
     },
     onwarn: (warning) => {
         if (warning.code === 'UNRESOLVED_IMPORT') return
