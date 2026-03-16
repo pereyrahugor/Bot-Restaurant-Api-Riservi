9dbf9f9cffb87adefc34dfd8caf43c35f2f1a04c
pereyrahugor <pereyrahugor@gmail.com>
2026-03-16 10:22:41 -0300

    Update errorReporter.ts

--- src/utils/errorReporter.ts
+++ src/utils/errorReporter.ts
@@ -1,19 +1,21 @@
-import { sendToGroup } from "./groupSender";
+import { EVENTS } from "@builderbot/bot";
+import { BaileysProvider } from "@builderbot/provider-baileys";
 
 class ErrorReporter {
+    private provider: BaileysProvider;
     private groupId: string;
 
-    constructor(provider: any, groupId: string) {
+    constructor(provider: BaileysProvider, groupId: string) {
+        this.provider = provider;
         this.groupId = groupId;
     }
 
     async reportError(error: Error, userId: string, userLink: string) {
-        const errorMessage = `⚠ pregunta que NO supe responder ⚠\n` +
-            `No supe: ${error.message}\n` +
+        const errorMessage = `⚠️ Falla de conexion OPEN IA ⚠️\nSe hicieron 5 reintento de establecer conexión.\n` +
             `whatsappLink = ${userLink}`;
 
         try {
-            await sendToGroup(this.groupId, errorMessage);
+            await this.provider.sendMessage(this.groupId, errorMessage, {});
         } catch (sendError) {
             console.error("Error al enviar el mensaje de error al grupo:", sendError);
         }
